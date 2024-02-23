ff1303cfc6ac9056dffe5184ab2cb44dc9a910d3
allinhoou <128159534+allinhoou@users.noreply.github.com>
2024-02-23 08:20:33 -0300

    Add files via upload

---
+++ alex/type-boolean.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+//console.log(primeiroNumero === segundoNumero)//
+
+//console.log(primeiroNumero > segundoNumero);//
+
+//console.log(primeiroNumero < segundoNumero);//
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+console.log(texto1 === texto2)
---
+++ alex/type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "olá, mundo!!";
+const texto2 = 'olá, mundo!!';
+const senha = "senhaSuperSgura";
+const numeros = "141432434";
+const citacao = 'Meu nome é ';
+const meuNome = "allan";
+
+//concatenaçao (+)
+
+console.log(citacao + meuNome)
